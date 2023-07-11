e1c4be3ebfb6ade0b2736087b57240e81bb82f9f
AhmetSIRIM <ahmet.sirim@outlook.com>
2023-07-11 19:29:18 +0300

    Replace Transaction Examples with Basic Navigation Examples

--- app/src/main/java/com/whale/navigationexample/FirstFragment.kt
+++ app/src/main/java/com/whale/navigationexample/FirstFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class FirstFragment : Fragment() {
 
@@ -12,8 +14,19 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.navigate_from_first_fragment).setOnClickListener {
+            findNavController().navigate(
+                FirstFragmentDirections
+                    .actionFirstFragmentToSecondFragment()
+            )
+        }
+
     }
 
 }
--- app/src/main/java/com/whale/navigationexample/ThirdFragment.kt
+++ app/src/main/java/com/whale/navigationexample/ThirdFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class ThirdFragment : Fragment() {
 
@@ -12,8 +14,19 @@ class ThirdFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_third, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.navigate_from_third_fragment).setOnClickListener {
+            findNavController().navigate(
+                ThirdFragmentDirections
+                    .actionThirdFragmentToFourthFragment()
+            )
+        }
+
     }
 
 }
