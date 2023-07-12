97db83915bc42f7daa8781e7e449d590d891c275
AhmetSIRIM <ahmet.sirim@outlook.com>
2023-07-12 14:35:00 +0300

    Add data transfer between destinations example

--- app/src/main/java/com/whale/navigationexample/FirstFragment.kt
+++ app/src/main/java/com/whale/navigationexample/FirstFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
@@ -21,14 +22,21 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.navigate_from_first_fragment).setOnClickListener {
+
+            val articleName =
+                view.findViewById<TextView>(R.id.text_view_article_name).text.toString()
+
             findNavController().navigate(
                 FirstFragmentDirections
-                    .actionFirstFragmentToSecondFragment()
+                    .actionFirstFragmentToSecondFragment(articleName + "\n\nAdditionally I came from ${this.javaClass.simpleName}")
             )
         }
 
         view.findViewById<Button>(R.id.navigate_from_first_fragment_to_nested).setOnClickListener {
-            findNavController().navigate(R.id.action_firstFragment_to_nestedNavigation)
+            findNavController().navigate(
+                FirstFragmentDirections
+                    .actionFirstFragmentToNestedNavigation()
+            )
         }
 
     }
--- app/src/main/java/com/whale/navigationexample/SecondFragment.kt
+++ app/src/main/java/com/whale/navigationexample/SecondFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 
 class SecondFragment : Fragment() {
+
+    private val args: SecondFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +23,8 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.text_view_article_name).text = args.articleTitle
 
         view.findViewById<Button>(R.id.navigate_from_second_fragment).setOnClickListener {
             findNavController().navigate(
